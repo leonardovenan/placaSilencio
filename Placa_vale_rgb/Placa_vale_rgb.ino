0642a167ed92957cb068093bd266ae292eeaab79
LEONARDO VENANCIO CORREIA <leonardovenancio12@gmail.com>
2019-06-17 08:04:32 -0300

    Add files via upload

---
+++ Placa_vale_rgb/Placa_vale_rgb.ino
@@ -0,0 +1,28 @@
+#define RED 5
+#define GREEN 9
+#define pinSom 4
+
+int i;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(pinSom, INPUT);
+
+  analogWrite(GREEN, 255);
+  analogWrite(RED, 0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(pinSom) == HIGH){
+    analogWrite(GREEN, 0);
+    for(i = 0; i <= 15; i++){
+      analogWrite(RED, 255);
+      delay(500);
+      analogWrite(RED, 0);
+    }
+    analogWrite(GREEN, 255);
+  }
+}
